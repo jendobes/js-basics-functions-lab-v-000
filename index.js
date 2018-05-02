46702cfa2b92997ceb2e1a8e7121eb392d41b11b
Jennifer Dobak <jdobak1@gmail.com>
2018-05-02 03:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function calculatesFarePrice(startBlock, endBlock) {
     case distance < 400:
     return fare = 0
 
-    case 400 < distance < 2000:
+    case distance > 400 && distance <= 2000:
     return fare = (distance - 400)*.02
 
     case distance > 2500:
