5e7bc5b6829c908c5cda57ce0412eb9c719746d4
Jennifer Dobak <jdobak1@gmail.com>
2018-05-02 03:17:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function distanceFromHqInFeet(block) {
   return distanceFromHqInBlocks(block) * 264
 }
 
-function distanceTravelledInFeet() {
-
+function distanceTravelledInFeet(startBlock, endBlock) {
+  return Math.abs(startBlock - endBlock)*264
 }
 
 function calculatesFarePrice() {
